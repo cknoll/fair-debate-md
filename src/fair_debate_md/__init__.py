480939e195d109e0a2259f193b777375acb23c96
Carsten Knoll <carsten.knoll@posteo.de>
2024-10-30 11:36:01 +0100

    __init__.py: robustify import logic

--- src/fair_debate_md/__init__.py
+++ src/fair_debate_md/__init__.py
@@ -1,9 +1,20 @@
 from .release import __version__
 
+# the package is imported during installation
+# however installation happens in an isolated build environment
+# where no dependencies are installed.
+
+# this means: no importing the following modules will fail
+# during installation this is OK
+
 try:
     from . import core
     from . import fixtures
     from .core import *
 except ImportError:
-    # necessary during installation
-    pass
+    import os
+    if "PIP_BUILD_TRACKER" in os.environ:
+        pass
+    else:
+        # raise the original exception
+        raise
